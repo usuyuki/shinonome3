f8f4f29c81582d1f308b49152fdfcbf22c10718b
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-09-26 11:41:51 +0900

    wip ルーティングの準備

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,33 @@ use App\Http\Controllers\Auth\LoginController;
 //     return view('welcome');
 // });
 
-Route::get('/',[IndexController::class,'index'])->name('index');
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+//あいさつ'/greet'
 Route::get('/greet',[GreetController::class,'showGreetPage'])->name('greet');
 Route::post('/greet',[GreetController::class,'postGreet'])->name('greet');
+//コチャ '/directmessage'
+// Route::get('/directmessage',[Controller::class,'directmessage'])->name('directmessage');
+//プロフィールと設定'/setting'
+// Route::get('/setting',[Controller::class,'setting'])->name('setting');
+//ヒストリーと起床時間'/record'
+// Route::get('/record',[Controller::class,'record'])->name('record');
+//プライバイシーポリシー '/privacypolicy'
+Route::get('/privacypolicy',[IndexController::class,'privacypolicy'])->name('privacypolicy');
+//ご利用にあたって '/explain'
+Route::get('/explain',[IndexController::class,'explain'])->name('explain');
+//お問い合わせ'/contact'
+Route::get('/contact',[IndexController::class,'contact'])->name('contact');
+//しののめの使い方 '/'
+Route::get('/',[IndexController::class,'index'])->name('index');
+
+
+
+
+//laravel/uiでのauth関連
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
 
 //socialiteのためのルーティング
 Route::get('/login/{provider}', [LoginController::class,'redirectToProvider']);
