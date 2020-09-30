9c95a5c0ed311cc9c814673f0100a10d15fa9d65
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-09-30 17:11:16 +0900

    feat GoogleAnalyst

--- resources/views/layouts/shinonome_in_app.blade.php
+++ resources/views/layouts/shinonome_in_app.blade.php
@@ -2,6 +2,19 @@
 <html>
 
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-179296536-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-179296536-1');
+    </script>
+
     <meta charset="utf-8" />
     <title>@yield('title')</title>
     <meta name="description" content="しののめは朝に特化した朝限定のSNSです。" />
