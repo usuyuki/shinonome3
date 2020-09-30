f353db9f5a0419d8af5ebe3b92624295fc65840c
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-09-30 10:24:00 +0900

    feat&fix favicon追加と他修正

---
+++ resources/views/components/explain.blade.php
@@ -0,0 +1,22 @@
+<!-- ご利用にあたってのためのコンポーネント -->
+<style>
+    .wrapper {
+        width: 70%;
+        margin: 0 auto;
+    }
+
+    .title {
+        width: 100%;
+        background-color: #FF8000;
+        color: white;
+    }
+</style>
+<div class="wrapper">
+
+    <div class="title">
+        <h2>　{{$title}}</h2>
+    </div>
+    <div class="explain">
+        <p>{{$explain}}</p>
+    </div>
+</div>
--- resources/views/main/right/explain.blade.php
+++ resources/views/main/right/explain.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.shinonome_out_app')
 
 @section('content')
-<p>工事中</p>
+<style>
+    h1 {
+        text-align: center;
+
+    }
+</style>
+<h1>ご利用にあたって</h1>
+@include('components.explain',
+['title'=>'JavaScript',
+'explain'=>'本サイトでは、より快適にご利用いただくため、JavaScript を使用しています。ご使用のブラウザ設定においてJavaScript をオン (有効) にされていない場合に、時計やグラフが正しく表示されない場合がありますので、ご了承ください。
+'])
+
+@include('components.explain',
+['title'=>'Cookie',
+'explain'=>'本サイトでは、ご利用者様が訪問された際、より便利に閲覧していただくため、Cookie を使用しております。Cookie は、本サイトの運用に関連するサーバから、ご利用者さまのブラウザに送信する情報で、ご利用者さまのコンピューターに記録されますが、ご利用者さまのコンピューターへ直接的な悪影響を及ぼすことはございません。
+ご本人の希望で「Cookie」の受け取りを、ブラウザで設定することにより拒否することができますが、ログインや投稿が利用できない場合がありますので、予めご了承ください。
+'])
+
+@include('components.explain',
+['title'=>'Google Analytics',
+'explain'=>'本サービスではGoogleによるアクセス解析ツール「GoogleAnalytics」を利用しています。このGoogleAnalyticsはトラフィックデータの収集のためにクッキー（Cookie）を使用しております。トラフィックデータは匿名で収集されており、個人を特定するものではありません。
+'])
+
+
 @endsection
