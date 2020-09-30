d87fb4ab77b60f40855585e6bc0a3f9c5f346bde
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-09-30 19:35:43 +0900

    feat 使い方ページ更新

--- resources/views/main/right/index.blade.php
+++ resources/views/main/right/index.blade.php
@@ -1,10 +1,23 @@
 @extends('layouts.shinonome_in_app')
 @section('title','しののめ')
 @section('content')
+<style>
+    .main-explain {
+        text-align: center;
+    }
+
+    .main-exp {
+        text-align: center;
+    }
+</style>
 <div class="main-title">
     <h1>しののめへようこそ</h1>
 </div>
+<div class="main-exp">
+    <P>しののめは朝に特化した朝限定のSNSです。</P>
+</div>
 <div class="main-explain">
-    <P>しののめは朝に特化した朝限定のSNSです。素敵な朝を過ごしましょう。</P>
+    <h2>使い方</h2>
+    <p>申し訳ありません。準備中です。</p>
 </div>
 @endsection
