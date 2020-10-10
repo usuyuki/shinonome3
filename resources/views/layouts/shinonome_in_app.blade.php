ff91d1a1440545eaa0ed4e27b1eed106161b402e
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-10-10 23:40:47 +0900

    fix ログアウトできない問題の解消

--- resources/views/layouts/shinonome_in_app.blade.php
+++ resources/views/layouts/shinonome_in_app.blade.php
@@ -230,16 +230,20 @@
                     </div>
                     <div class="logout-btn">
 
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                            ログアウト
-                        </a>
-                        <form id="logout-btn" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                ログアウト
+                            </a>
+    
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
                     </div>
 
                     @endguest
+
                     @yield('left')
                 </div>
             </div>
