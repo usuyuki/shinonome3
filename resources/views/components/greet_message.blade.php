a6e209ec9bf1542ff3971498c2f7c38fcf111f2b
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-10-11 16:18:25 +0900

    wip あいさつページ改装

--- resources/views/components/greet_message.blade.php
+++ resources/views/components/greet_message.blade.php
@@ -1,4 +1,120 @@
 <style>
+.greet-wrapper{
+    margin:20px auto;
+}
+.greet-content{
+    border:1px solid black;
+    width: 800px;
+    background-color:#87C9B6; 
+    border-radius: 2%;
+
+
+}
+.greet-top{
+    width: 800px;
+    height:80px;
+    border:1px solid black;
+}
+
+.greet-center{
+    border:1px solid black;
+    text-align: center;
+    /* margin:40px 40px; */
+    width: 800px;
+   
+}
+.greet-bottom{
+    width: 800px;
+    border:1px solid black;
+    text-align: right;
+
+}
+.greet-bottom p{
+    margin-bottom: 0px;
+}
+.greet-icon{
+    float: left;
+    /* border: 1px solid blue; */
+    margin:20px 20px;
+
+    background-position: center ;
+    
+
+}
+.greet-icon img{
+    border-radius: 50%;  /* 角丸半径を50%にする(=円形にする) */
+}
+
+.greet-name{
+    margin:20px 0;
+    float:left;
+}
+.greet-name a{
+    text-decoration: none;
+    
+    color:black;
+}
+.greet-timestamp{
+    text-align: right;
+    margin:20px 20px;
+
+}
+.greet-delate{
+    float:left;
+    margin-top:20px;
+    margin-left:400px;
+}
+.greet-like{
+    position: relative;
+    top:20px;
+    left:400px;
+}
+.like-btn{
+    
+}
+
 
 </style>
 
+<div class="greet-wrapper">
+
+    <div class="greet-content">
+         <!-- 上枠 -->
+        <div class="greet-top">
+
+            <div class="greet-icon">
+                {{$icon}}
+            </div>
+            
+            <div class="greet-name">
+                <p>{{$name}}</p>
+            </div>
+            <div class="greet-delate">
+                {{$delate}}
+            </div>
+            
+            <div class="greet-timestamp">
+                <p>{{$timestamp}}</p>
+            </div>
+            
+            
+        </div>
+         <!-- 中間枠-->
+        <div class="greet-center">
+              <!-- 本文-->
+            <p>{{$text}}</p>
+        </div>
+        <!-- 底枠-->
+        <div class="greet-bottom">
+              <!-- 好きされた数-->
+                <p>トータル好き:{{$like_count}}</p>
+
+        </div>
+            
+            
+        <div class="greet-like">
+                {{$like_btn}}
+        </div>
+    </div>
+
+</div>
--- resources/views/greet/index.blade.php
+++ resources/views/greet/index.blade.php
@@ -1,8 +1,13 @@
-{{-- @extends('layouts.shinonome_in_app')
-@section('title','ご利用にあたって/しののめ') --}}
-@extends('layouts.app')
-@section('content')
+{{-- @extends('layouts.app') --}}
+@extends('layouts.shinonome_in_app')
+@section('title','あいさつ/しののめ')
 
+@section('content')
+<style>
+/* img{
+    border-radius: 50%;  /* 角丸半径を50%にする(=円形にする) */
+} */
+</style>
  <!-- 投稿コーナー -->
 <div class="card-body">
     <form method="POST" action="/greet">
@@ -52,71 +57,78 @@
                 <div class="col-md-8 mb-3">
                     <div class="card">
                         <div class="card-haeder p-3 w-100 d-flex">
-                            {{-- @component('components.explain')
-        @slot('title')
-        {{$timeline->greet}}
-        @endslot
-        @slot('explain')
-        これはメッセージの表示です。
-        @endslot
-    @endcomponent --}}
-                            <img src="{{$timeline->user->profile_photo_path }}" class="rounded-circle" width="50" height="50">
-                            <div class="ml-2 d-flex flex-column">
+                            @component('components.greet_message')
+                                @slot('icon')
+                                    <img src="{{$timeline->user->profile_photo_path }}" class="rounded-circle" width="50" height="50">
+                                @endslot
 
-                                 <a href="{{ url('users/' .$timeline->user->id) }}" class="text-secondary"><p class="mb-0">{{ $timeline->user->name }}</p></a>
-                            </div>
-                            <div class="d-flex justify-content-end flex-grow-1">
-                                <p class="mb-0 text-secondary">{{ $timeline->created_at->format('Y-m-d H:i') }}</p>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            {!! nl2br(e($timeline->greet)) !!}
-                        </div>
-                      
-                        <!-- あいさつ削除コーナー -->
-                        <div class="card-footer py-1 d-flex justify-content-end bg-white">
-                            @if ($timeline->user->id === Auth::user()->id)
-                                <div class="dropdown mr-3 d-flex align-items-center">
-                                    <a href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fas fa-ellipsis-v fa-fw"></i>
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                        <form method="POST" action='/greet/delate/?greet_id={{$timeline->id}}' class="mb-0">
-                                            @csrf
-                                            {{-- @method('DELETE') --}}
+                                @slot('name')
+                                    <a href="{{ url('users/' .$timeline->user->id) }}" class="text-secondary"><p class="mb-0">{{ $timeline->user->name }}</p></a>
+                                @endslot
 
-                                           
-                                            <button type="submit" class="dropdown-item del-btn">削除</button>
-                                        </form>
+                                @slot('timestamp')
+                                    <p>{{ $timeline->created_at->format('Y-m-d H:i') }}</p>
+                                @endslot
+
+                                @slot('text')
+                                    <p>{{$timeline->greet}}</p>
+                                @endslot
+
+                                @slot('delate')
+                                <!-- あいさつ削除コーナー -->
+                                    @if ($timeline->user->id === Auth::user()->id)
+                                    <div class="dropdown mr-3 d-flex align-items-center">
+                                        <a href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i class="fas fa-ellipsis-v fa-fw"></i>
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                            <form method="POST" action='/greet/delate/?greet_id={{$timeline->id}}' class="mb-0">
+                                                @csrf
+                                                {{-- @method('DELETE') --}}
+
+                                            
+                                                <button type="submit" class="dropdown-item del-btn">        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z"/></svg>
+                                                </button>
+                                            </form>
+                                        </div>
                                     </div>
-                                </div>
-                            @endif
+                                    @endif
 
+                                @endslot
 
-                           <!-- いいねコーナー -->
-                           <div class="d-flex align-items-center">
-                            @if (!in_array($user->id, array_column($timeline->favorites->toArray(), 'user_id'), TRUE))
-                                <form method="POST" action="{{ url('favorites/') }}" class="mb-0">
-                                    @csrf
+                                @slot('like_btn')
+                                    <!-- いいねコーナー -->
+                                    <div class="d-flex align-items-center">
+                                        @if (!in_array($user->id, array_column($timeline->favorites->toArray(), 'user_id'), TRUE))
+                                            <form method="POST" action="{{ url('favorites/') }}" class="mb-0">
+                                                @csrf
 
-                                    <input type="hidden" name="greet_id" value="{{ $timeline->id }}">
-                                    <button type="submit" class="btn p-0 border-0 text-primary"><i class="far fa-heart fa-fw"></i></button>
-                                </form>
-                            @else
-                                <form method="POST" action="{{ url('favorites/' .array_column($timeline->favorites->toArray(), 'id', 'user_id')[$user->id]) }}" class="mb-0">
-                                    @csrf
-                                    @method('DELETE')
+                                                <input type="hidden" name="greet_id" value="{{ $timeline->id }}">
+                                                <button type="submit" class="btn p-0 border-0 text-primary"><i class="far fa-heart fa-fw">  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>                                                    
+                                                </i></button>
+                                            </form>
 
-                                    <button type="submit" class="btn p-0 border-0 text-danger"><i class="fas fa-heart fa-fw"></i></button>
-                                </form>
-                            @endif
-                            <p class="mb-0 text-secondary">{{ count($timeline->favorites) }}</p>
+                                        @else
+                                            <form method="POST" action="{{ url('favorites/' .array_column($timeline->favorites->toArray(), 'id', 'user_id')[$user->id]) }}" class="mb-0">
+                                                @csrf
+                                                @method('DELETE')
+
+                                                <button type="submit" class="btn p-0 border-0 text-danger"><i class="fas fa-heart fa-fw"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>      
+                                                </i></button>
+                                            </form>
+                                        </div>
+                                        @endif
+                                @endslot
+                                        
+                                @slot('like_count')
+                                        {{ count($timeline->favorites) }}
+                                @endslot
+
+                            @endcomponent
+                            
+
                         </div>
 
-
-
-                        </div>
-                    </div>
                 </div>
             @endforeach
         @endif
